27c13acc8128d89585ea776da4c25a3e1153f5ff
Totr <toantmse170274@fpt.edu.vn>
2024-10-18 07:37:30 +0700

    add appointmentService

--- HairSalon/HairSalon.Api/Controllers/v1/AppointmentsController.cs
+++ HairSalon/HairSalon.Api/Controllers/v1/AppointmentsController.cs
@@ -14,84 +14,84 @@ namespace HairSalon.Api.Controllers.v1
             _context = context;
         }
 
-        // GET: api/Appointments
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<Appointment>>> GetAppointments()
-        //{
-        //    return await _context.Appointments.ToListAsync();
-        //}
+       //GET: api/Appointments
+       [HttpGet]
+        public async Task<ActionResult<IEnumerable<Appointment>>> GetAppointments()
+        {
+            return await _context.Appointments.ToListAsync();
+        }
 
-        // GET: api/Appointments/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Appointment>> GetAppointment(int id)
-        //{
-        //    var appointment = await _context.Appointments.FindAsync(id);
+        //GET: api/Appointments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Appointment>> GetAppointment(int id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
 
-        //    if (appointment == null)
-        //    {
-        //        return NotFound();
-        //    }
+            if (appointment == null)
+            {
+                return NotFound();
+            }
 
-        //    return appointment;
-        //}
+            return appointment;
+        }
 
-        // PUT: api/Appointments/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutAppointment(int id, Appointment appointment)
-        //{
-        //    if (id != appointment.Id)
-        //    {
-        //        return BadRequest();
-        //    }
+        //PUT: api/Appointments/5
+        //To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAppointment(int id, Appointment appointment)
+        {
+            if (id != appointment.Id)
+            {
+                return BadRequest();
+            }
 
-        //    _context.Entry(appointment).State = EntityState.Modified;
+            _context.Entry(appointment).State = EntityState.Modified;
 
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!AppointmentExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppointmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
-        //    return NoContent();
-        //}
+            return NoContent();
+        }
 
-        // POST: api/Appointments
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPost]
-        //public async Task<ActionResult<Appointment>> PostAppointment(Appointment appointment)
-        //{
-        //    _context.Appointments.Add(appointment);
-        //    await _context.SaveChangesAsync();
+        //POST: api/Appointments
+        //To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Appointment>> PostAppointment(Appointment appointment)
+        {
+            _context.Appointments.Add(appointment);
+            await _context.SaveChangesAsync();
 
-        //    return CreatedAtAction("GetAppointment", new { id = appointment.Id }, appointment);
-        //}
+            return CreatedAtAction("GetAppointment", new { id = appointment.Id }, appointment);
+        }
 
-        // DELETE: api/Appointments/5
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteAppointment(int id)
-        //{
-        //    var appointment = await _context.Appointments.FindAsync(id);
-        //    if (appointment == null)
-        //    {
-        //        return NotFound();
-        //    }
+        //DELETE: api/Appointments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAppointment(int id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
 
-        //    _context.Appointments.Remove(appointment);
-        //    await _context.SaveChangesAsync();
+            _context.Appointments.Remove(appointment);
+            await _context.SaveChangesAsync();
 
-        //    return NoContent();
-        //}
+            return NoContent();
+        }
 
         private bool AppointmentExists(int id)
         {
---
+++ HairSalon/HairSalon.Core/Dtos/Responses/AppointmentViewResponse.cs
@@ -0,0 +1,37 @@
+﻿using HairSalon.Core.Entities;
+using HairSalon.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairSalon.Core.Dtos.Responses
+{
+    public class AppointmentViewResponse
+    {
+        //public int Id { get; set; }
+
+        //public int CustomerId { get; set; }
+        public string? CustomerName { get; set; }
+
+        //public int StylistId { get; set; }
+        public string? StylistName { get; set; }
+
+        public DateOnly AppointmentDate { get; set; }
+
+        public TimeOnly StartTime { get; set; }
+
+        public TimeOnly EndTime { get; set; }
+
+        [StringLength(255)]
+        public string? Note { get; set; }
+
+        public AppointmentStatus AppointmentStatus { get; set; }
+
+        public ICollection<AppointmentService> AppointmentServices { get; set; } = new List<AppointmentService>();
+
+        public decimal? AppointmentCost { get; set; }
+    }
+}
