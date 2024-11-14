6bae799021018a6cbd9a1924c39dfc8d37a07296
HoangTien0105 <hoangtien01052003@gmail.com>
2024-11-14 20:26:56 +0700

    clear session after payment

--- HairSalon/HairSalon.Web/Pages/Payments/PaymentFailed.cshtml.cs
+++ HairSalon/HairSalon.Web/Pages/Payments/PaymentFailed.cshtml.cs
@@ -5,8 +5,17 @@ namespace HairSalon.Web.Pages.Payments
 {
     public class PaymentFailedModel : PageModel
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public PaymentFailedModel(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public void OnGet()
         {
+            _httpContextAccessor.HttpContext.Session.Remove("CustomerId");
+            _httpContextAccessor.HttpContext.Session.Remove("AppointmentId");
         }
     }
 }
--- HairSalon/HairSalon.Web/Pages/Payments/PaymentSuccess.cshtml.cs
+++ HairSalon/HairSalon.Web/Pages/Payments/PaymentSuccess.cshtml.cs
@@ -5,8 +5,17 @@ namespace HairSalon.Web.Pages.Payments
 {
     public class PaymentSuccessModel : PageModel
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public PaymentSuccessModel(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public void OnGet()
         {
+            _httpContextAccessor.HttpContext.Session.Remove("CustomerId");
+            _httpContextAccessor.HttpContext.Session.Remove("AppointmentId");
         }
     }
 }
